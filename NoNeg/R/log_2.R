de0a3c4d367f4e79bd46883775c57526f06fba6b
Celeste <cpicone99@gmail.com>
2023-03-06 16:31:37 -0500

    Create log_2.R

---
+++ NoNeg/R/log_2.R
@@ -0,0 +1,18 @@
+#' log.2
+#'
+#' A way to return errors for log of negative numbers instead of NAs
+#'
+#' @param x A number
+#'
+#' @return An error or a number
+#' @export
+#'
+#' @examples
+#' log.2(1)
+log.2 = function(x){
+  if ((x) < 0){
+    rlang::abort(message="negative input, NA introduced!",x=x)
+  } else {
+    log(x)
+  }
+}
